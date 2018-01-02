60b0497c91a5910f38bba5cedafae993332fa314
yujc <jch_yu@hotmail.com>
2018-01-03 07:25:22 +0800

    test

--- src/app/components/factory.comp.ts
+++ src/app/components/factory.comp.ts
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import Headbar from "./headbar/headbar.vue";
 import Footbar from "./footbar/footbar.vue";
-import Calandar from "./calandar/calandar.vue";
 
 // SGV-BUILD-COMP-FAC # NOT DELETE
-Vue.component("calandar", require("./calandar/calandar.vue").default);
 Vue.component("footbar", Footbar);
 Vue.component("headbar", Headbar);
 
